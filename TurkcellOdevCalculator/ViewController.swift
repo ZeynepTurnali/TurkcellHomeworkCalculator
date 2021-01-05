caa3c78c0178b60eb6d437d6f4516978a8a8dc47
Zeynep Turnalı <zeynepturnali23@gmail.com>
2021-01-05 23:42:35 +0300

    first commit

--- TurkcellOdevCalculator/ViewController.swift
+++ TurkcellOdevCalculator/ViewController.swift
@@ -1,19 +1,73 @@
 //
 //  ViewController.swift
 //  TurkcellOdevCalculator
-//
-//  Created by testinium on 5.01.2021.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstNumberText: UITextField!
+    
+    @IBOutlet weak var secondNumberText: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func sumButton(_ sender: UIButton) {
+        calculate(operation: "sum")
+    }
+    
+    
+    @IBAction func substractButton(_ sender: UIButton) {
+        calculate(operation: "substract")
+    }
+    
+    
+    @IBAction func productButton(_ sender: UIButton) {
+        calculate(operation: "product")
+    }
+    
+    @IBAction func devideButton(_ sender: UIButton) {
+        calculate(operation: "divide")
+    }
+    
+    func calculate(operation: String){
+        if let firstNumber = Int(firstNumberText.text!), let secondNumber = Int(secondNumberText.text!){
+            switch operation {
+            case "sum":
+                result = firstNumber + secondNumber
+                resultLabel.text = "Result is: \(result)"
+            case "substract":
+                result = firstNumber - secondNumber
+                resultLabel.text = "Result is: \(result)"
+            case "product":
+                result = firstNumber * secondNumber
+                resultLabel.text = "Result is: \(result)"
+            case "divide":
+                if secondNumber != 0 {
+                    result = firstNumber / secondNumber
+                    resultLabel.text = "Result is: \(result)"
+                } else {
+                    resultLabel.text = "You can not divide a number by 0"
+                }
+            default:
+                print("You entered wrong operation!")
+            }
+        }else {
+            print("bad input")
+            resultLabel.text = "Please enter a valid input"
+        }
+        
+        
+    }
+    
 }
 
